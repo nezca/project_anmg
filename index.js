b6642c92c1df932756815b62ea16d12f829362a9
nezca <oldyth@gmail.com>
2016-12-07 13:34:10 +0900

    reload

--- index.js
+++ index.js
@@ -11,32 +11,37 @@ app.set('view engine','pug');
 app.set('views', './views');
 
 //----------- MySQL setting -----------
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-04.cleardb.net', 
-  user     : 'b69910662a1301',
-  password : '0c76890f',
-  database : 'heroku_36ce9bdde949664'
-});
-
-connection.connect();
+//var mysql      = require('mysql');
+//var connection = mysql.createConnection({
+//  host     : 'us-cdbr-iron-east-04.cleardb.net', 
+//  user     : 'b69910662a1301',
+//  password : '0c76890f',
+//  database : 'heroku_36ce9bdde949664'
+//});
+//
+//connection.connect();
 
 //------------ Pages Routing -----------------------------
 app.get('/', function(req,res){
     res.render('intro');
 });
 
-//------------ pages routinh for MySQL Test --------------
 app.get('/home', function(req,res){
-  var sql = 'SELECT id,title FROM topic';
-  connection.query(sql, function(err, topics, fields){
-    if(err){
-            console.log(err);
-            res.status(500).send('what the fuck you!');
-        }
-    res.render('add',{topics:topics});
-  });  
+    res.render('intro');
 });
+
+
+//------------ pages routinh for MySQL Test --------------
+//app.get('/home', function(req,res){
+//  var sql = 'SELECT id,title FROM topic';
+//  connection.query(sql, function(err, topics, fields){
+//    if(err){
+//            console.log(err);
+//            res.status(500).send('what the fuck you!');
+//        }
+//    res.render('add',{topics:topics});
+//  });  
+//});
 
 //----- node.js tutorial's app.listen method -----
 app.listen(app.get('port'), function() {
