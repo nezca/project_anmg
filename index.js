54aa777ca6bceccc3fa23408fedad94a2007d612
nezca <oldyth@gmail.com>
2016-12-07 11:35:34 +0900

    161207_2

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+//------------ npm express setting --------------
 var express = require('express');
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static('public'));
 
+//------------ local server port setting ---------
+app.set('port', (process.env.PORT || 5000));
 
-//----------- pug(former jade) setting -----------
+//----------- pug(former 'jade') setting -----------
 app.set('view engine','pug');
 app.set('views', './views');
 
+//------------ Pages Routing -----------------------------
 app.get('/', function(req,res){
     res.render('intro');
 });
-
 
 //----- node.js tutorial's app.listen method -----
 app.listen(app.get('port'), function() {
