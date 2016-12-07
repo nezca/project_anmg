d692a4e467463f9ed5329986539de76ad6af858a
nezca <oldyth@gmail.com>
2016-12-07 12:28:26 +0900

    SQL

--- index.js
+++ index.js
@@ -10,9 +10,32 @@ app.set('port', (process.env.PORT || 5000));
 app.set('view engine','pug');
 app.set('views', './views');
 
+//----------- MySQL setting -----------
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-04.cleardb.net', 
+  user     : 'b69910662a1301',
+  password : '0c76890f',
+  database : 'heroku_36ce9bdde949664'
+});
+
+connection.connect();
+
 //------------ Pages Routing -----------------------------
 app.get('/', function(req,res){
     res.render('intro');
+});
+
+//------------ pages routinh for MySQL Test --------------
+app.get('/home', function(req,res){
+  var sql = 'SELECT id,title FROM topic';
+  connection.query(sql, function(err, topics, fields){
+    if(err){
+            console.log(err);
+            res.status(500).send('what the fuck you!');
+        }
+    res.render('add',{topics:topics});
+  });  
 });
 
 //----- node.js tutorial's app.listen method -----
