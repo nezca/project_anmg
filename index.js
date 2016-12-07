cefa7e9bdba39f419515bdaf5aee0b7284ff5b86
nezca <oldyth@gmail.com>
2016-12-07 21:36:14 +0900

    test

--- index.js
+++ index.js
@@ -10,38 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 app.set('view engine','pug');
 app.set('views', './views');
 
-//---------- ■ MySQL setting method_#1 -----------
-//var mysql      = require('mysql');
-//var db_config = {
-//  host     : 'us-cdbr-iron-east-04.cleardb.net', 
-//  user     : 'b69910662a1301',
-//  password : '0c76890f',
-//  database : 'heroku_36ce9bdde949664'
-//};
-//
-//var connection;
-//function handleDisconnect(){
-//  connection = mysql.createConnection(db_config); 
-//  connection.connect(function(err) {              
-//    if(err) {
-//      console.log('error when connecting to db:', err);
-//      setTimeout(handleDisconnect, 2000);
-//    }
-//  });                                     
-//                                         
-//  connection.on('error', function(err) {
-//    console.log('db error', err);
-//    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-//      handleDisconnect();                         
-//    } else {                                      
-//      throw err;                                  
-//    }
-//  });
-//};
-
-//handleDisconnect();
-
-//---------- ■ MySQL setting method_#2 (recommend) ------
+//--------- ClearDB(mysql) Setting -------------------
 var mysql      = require('mysql');
 var db_config = {
   host     : 'us-cdbr-iron-east-04.cleardb.net', 
@@ -49,28 +18,31 @@ var db_config = {
   password : '0c76890f',
   database : 'heroku_36ce9bdde949664'
 };
-var handleKFDisconnect = function() {
-    kfdb.on('error', function(err) {
-        if (!err.fatal) {
-            return;
-        }
-        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-            console.log("PROTOCOL_CONNECTION_LOST");
-            throw err;
-        }
-        log.error("The database is error:" + err.stack);
+var connection;
 
-        kfdb = mysql.createConnection(kf_config);
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {              
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }                                     
+  });                                     
+                                          
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();                         
+    } else {
+      throw err;                                  
+    }
+  });
+}
 
-        console.log("kfid");
 
-        console.log(kfdb);
-        handleKFDisconnect();
-    });
-   };
-//   handleKFDisconnect();
+handleDisconnect();
 
-//------------ Pages Routing -----------------------------
+//------------ Pages Routing -------------------------
 app.get('/', function(req,res){
     res.render('intro');
 });
@@ -79,18 +51,17 @@ app.get('/home', function(req,res){
     res.render('intro');
 });
 
-
-//------------ pages routinh for MySQL Test --------------
-//app.get('/home', function(req,res){
-//  var sql = 'SELECT id,title FROM topic';
-//  connection.query(sql, function(err, topics, fields){
-//    if(err){
-//            console.log(err);
-//            res.status(500).send('what the fuck you!');
-//        }
-//    res.render('add',{topics:topics});
-//  });  
-//});
+//------------ pages routinh for MySQL Test ----------
+app.get('/add', function(req,res){
+  var sql = 'SELECT id,title FROM topic';
+  connection.query(sql, function(err, topics, fields){
+    if(err){
+            console.log(err);
+            res.status(500).send('what the fuck you!');
+        }
+    res.render('add',{topics:topics});
+  });  
+});
 
 //----- node.js tutorial's app.listen method -----
 app.listen(app.get('port'), function() {
