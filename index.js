9cd286a5f4244576d93518f61453642d1b942df5
nezca <oldyth@gmail.com>
2016-12-29 15:04:30 +0900

    heroku test

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //------------ npm express setting --------------
 var express = require('express');
 var app = express();
+var multer = require('multer');
 app.use(express.static('public'));
+var bootstrap = require("express-bootstrap-service");
+var bodyPaser = require('body-parser');
 
 //------------ local server port setting ---------
 app.set('port', (process.env.PORT || 5000));
@@ -9,6 +12,8 @@ app.set('port', (process.env.PORT || 5000));
 //----------- pug(former 'jade') setting -----------
 app.set('view engine','pug');
 app.set('views', './views');
+app.use(bootstrap.serve);
+app.use(bodyPaser.urlencoded({extended:false}));
 
 //--------- ClearDB(mysql) Setting -------------------
 var mysql      = require('mysql');
@@ -43,24 +48,22 @@ function handleDisconnect() {
 handleDisconnect();
 
 //------------ Pages Routing -------------------------
+    
 app.get('/', function(req,res){
-    res.render('intro');
+  res.render('intro');
 });
 
-app.get('/portfolio', function(req,res){
-    res.render('portfolio');
-});
-
-//------------ pages routinh for MySQL Test ----------
-app.get('/DBTest', function(req,res){
-  var sql = 'SELECT id,title FROM topic';
-  connection.query(sql, function(err, topics, fields){
+app.post('/return', function(req,res){
+  var distance = req.body.course_distance;
+  var difficulty = req.body.course_difficulty;
+  var sql = 'SELECT * FROM course_test HAVING course_distance < ? and course_difficulty < ?';
+  connection.query(sql,[distance,difficulty], function(err, trekking, fields){
     if(err){
-            console.log(err);
-            res.status(500).send('what the fuck you!');
-        }
-    res.render('DBTest',{topics:topics});
-  });  
+      console.log(err);
+      res.status(500).send('what the hell!');
+      }
+    res.render('return',{trekking:trekking});   
+  });
 });
 
 //----- node.js tutorial's app.listen method -----
